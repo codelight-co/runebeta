c960feb36b22361070eff98ca9dafd5d2e5b413d
mison201 <mison201@gmail.com>
2024-03-18 13:38:18 +0700

    chore: add db example

--- service/src/modules/transactions/transaction.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Entity } from 'typeorm';
-import BaseTable from '../database/base-table';
-
-@Entity()
-export class Transaction extends BaseTable {
-  @Column({ nullable: true })
-  description?: string;
-}
