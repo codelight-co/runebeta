b8f40ba6f89e7dd329659f957d04990cca934f90
iota9star <iota9star@foxmail.com>
2024-03-20 00:45:48 +0800

    ✅ Add runes test

---
+++ rune_lib/tests/mempool.ts
@@ -0,0 +1,39 @@
+import {Network} from "bitcoinjs-lib";
+import {bitcoin} from "./bitcoin";
+
+
+function isTestnet(network: Network) {
+    return network === bitcoin.networks.testnet;
+}
+
+export interface UTXO {
+    txid: string;
+    vout: number;
+    value: number;
+}
+
+function mempoolUrl(networks: Network) {
+    return isTestnet(networks) ? 'https://mempool.space/testnet/api' : 'https://mempool.space/api';
+}
+
+export function getUTXOs(address: string, network: Network = bitcoin.networks.testnet) {
+    const url = mempoolUrl(network);
+    return fetch(`${url}/address/${address}/utxo`)
+        .then(res => res.json())
+        .then((v: UTXO[]) => v.map((e) => {
+            delete (e as any).status;
+            return e;
+        }).sort((a, b) => b.value - a.value));
+}
+
+
+export function broadcast(rawhex: string, network: Network) {
+    const url = mempoolUrl(network);
+    return fetch(`${url}/tx`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(rawhex),
+    }).then(response => response.text());
+}
