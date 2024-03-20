fb60b1f032f7239181807dc9c56a04e284a7f061
iota9star <iota9star@foxmail.com>
2024-03-21 00:27:20 +0800

    ✅ Add runes test more

--- rune_lib/tests/issue_mint_transfer_burn.test.ts
+++ rune_lib/tests/issue_mint_transfer_burn.test.ts
@@ -19,16 +19,98 @@ describe('Issue/Mint/Transfer/Burn', () => {
         address
     });
 
-    it('test issue tokens', async () => {
+    it('test issue tokens(fixedcap)', async () => {
         let utxos = await getUTXOs(address, network);
         console.table(utxos);
+        // the name of the token
+        let rune = Rune.fromString("HELLOFIXEDCAP");
         const runeStone = new RuneStone(
-            [{
-                id: BigInt(0),
-                amount: BigInt(1000000),
-                output: BigInt(1),
-            } as any], // edicts
-            new Etching(0, new Mint(BigInt(Date.now() * 1e6), BigInt(1000000), BigInt(2582653 + 10000)), null, 'xyzd', BigInt(0)), // etching
+            [
+                // if you want to mint tokens when issuing a token, then you need to pass this parameter.
+                {
+                    // always 0
+                    id: BigInt(0),
+                    // amount to mint
+                    amount: BigInt(1e10),
+                    // the amount to be minted will be on which output?
+                    output: BigInt(1),
+                } as any
+            ], // edicts
+            new Etching(
+                // like decimals.
+                2,
+                new Mint(null, BigInt(1e12), null),
+                // the name of the token, if null, it will be automatically generated.
+                rune,
+                // this is not the name of the token, only one character is supported here
+                '$',
+                BigInt(0)
+            ), // etching
+            false, // is burning? true/false
+            BigInt(0), // claim
+            null, // default output
+        );
+        runeStone.setTag('RUNE_TEST');
+        const encipher = runeStone.encipher();
+        const outputs = [
+            {
+                script: encipher,
+                value: 0,
+            },
+            {
+                // receiver
+                script: output,
+                value: 1,
+            }
+        ];
+        let amount = outputs.reduce((a, b) => a + b.value, 0);
+        const {inputs, fee, change} = tryTaprootTxInputs(utxos, amount, feeRate, 0, outputs.length);
+        console.table({amount, fee, change});
+        if (change) {
+            outputs.push({
+                script: output,
+                value: change
+            });
+        }
+        let psbt = toTaprootPSBT(inputs, outputs, output, pubkey, network);
+        let signed = signTaprootPSBT(signer, psbt, pubkey);
+        let tx = signed.extractTransaction();
+        console.table({
+            txid: tx.getId(),
+            fee: psbt.getFee(),
+            feeRate: psbt.getFeeRate(),
+        });
+        const rawhex = tx.toHex();
+        console.log(rawhex);
+        let stone = RuneStone.fromTransaction(tx);
+        console.log(stone);
+        // let txid = await broadcast(rawhex, network);
+        // console.log(txid);
+    })
+
+    it('test issue tokens(fairmint)', async () => {
+        let utxos = await getUTXOs(address, network);
+        console.table(utxos);
+        let rune = Rune.fromString("HELLOEVA");
+        const runeStone = new RuneStone(
+            [],
+            new Etching(
+                // like decimals.
+                8,
+                new Mint(
+                    // mint end time.
+                    BigInt(Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60),
+                    // maximum amount for each mint.
+                    BigInt(1e8),
+                    // the block at which minting ends.
+                    BigInt(1e8),
+                ),
+                // the name of the token, if null, it will be automatically generated.
+                rune,
+                // this is not the name of the token, only one character is supported here
+                '$',
+                BigInt(0)
+            ), // etching
             false, // is burning? true/false
             null, // claim
             null, // default output
@@ -74,16 +156,12 @@ describe('Issue/Mint/Transfer/Burn', () => {
         let utxos = await getUTXOs(address, network);
         console.table(utxos);
         // etching block height | etching transaction index
-        let runeId = new RuneId(2582645, 839);
+        let runeId = new RuneId(2582757, 389);
         const runeStone = new RuneStone(
-            [{
-                id: RuneId.toBigInt(runeId),
-                amount: BigInt(1000),
-                output: BigInt(1),
-            } as any], // edicts
+            [], // edicts
             null, // etching
             false, // is burning? true/false
-            null, // claim
+            RuneId.toBigInt(runeId), // claim
             null, // default output
         );
         runeStone.setTag('RUNE_TEST');
@@ -95,7 +173,7 @@ describe('Issue/Mint/Transfer/Burn', () => {
             },
             {
                 script: output,
-                value: 10,
+                value: 1,
             }
         ];
         let amount = outputs.reduce((a, b) => a + b.value, 0);
