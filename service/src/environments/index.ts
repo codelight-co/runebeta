afd9ac4584bb8fb3d91134682ad7c28712ea4498
mison201 <mison201@gmail.com>
2024-03-22 15:20:24 +0700

    chore: manal add env for test

--- service/src/environments/index.ts
+++ service/src/environments/index.ts
@@ -14,9 +14,12 @@ const DATABASE_TYPE: string = process.env.DATABASE_TYPE || NEED_TO_CONFIGURED;
 const DATABASE_HOST: string = process.env.DATABASE_HOST || NEED_TO_CONFIGURED;
 // const DATABASE_NAME: string = process.env.DATABASE_NAME || NEED_TO_CONFIGURED;
 const DATABASE_NAME: string = 'runebeta_debug';
-const DATABASE_USER: string = process.env.DATABASE_USER || NEED_TO_CONFIGURED;
-const DATABASE_PASSWORD: string =
-  process.env.DATABASE_PASSWORD || NEED_TO_CONFIGURED;
+// const DATABASE_USER: string = process.env.DATABASE_USER || NEED_TO_CONFIGURED;
+const DATABASE_USER: string = 'postgres';
+
+// const DATABASE_PASSWORD: string = process.env.DATABASE_PASSWORD || NEED_TO_CONFIGURED;
+const DATABASE_PASSWORD: string = 'Codelight123';
+
 const DATABASE_PORT: number = +process.env.DATABASE_PORT || 5432;
 
 // Redis cache
